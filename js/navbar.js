42c7949dedbb943117ec7f0b0694b28bd3bef5bf
CaptainMentallic <captainmentallic@outlook.com>
2025-02-07 18:55:57 -0700

    navbar additon

---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+var navbar = document.getElementById('navbar');
+var prevScrollPos = window.pageYOffset;
+var scrollUpThreshold = 100; // Threshold for upward scroll before showing navbar
+var accumulatedScrollUp = 0;
+
+window.addEventListener('scroll', function () {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos || currentScrollPos < scrollUpThreshold) {
+        // Scrolling up
+        accumulatedScrollUp += prevScrollPos - currentScrollPos;
+        if (accumulatedScrollUp > scrollUpThreshold || currentScrollPos < scrollUpThreshold) {
+            navbar.style.top = "0";
+            accumulatedScrollUp = 0; // Reset after showing navbar
+        }
+    } else {
+        // Scrolling down
+        navbar.style.top = "-100px";
+        accumulatedScrollUp = 0; // Reset if scrolling down
+    }
+
+    prevScrollPos = currentScrollPos;
+});
