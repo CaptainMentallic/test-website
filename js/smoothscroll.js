b72b52df3561ab27c987e3bce4cdbd9434052924
CaptainMentallic <captainmentallic@outlook.com>
2025-02-08 18:51:32 -0700

    scaling

---
+++ js/smoothscroll.js
@@ -0,0 +1,11 @@
+// For support with older browsers that dont have scroll-behaviour: smooth
+// This script will add smooth scroll to all anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
